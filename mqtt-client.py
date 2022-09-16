8bb992ef4925c5c2ef437fa3cd928334d2ce0a94
Chris Capobianco <ccapo@users.noreply.github.com>
2022-09-16 01:15:36 -0400

    Update mqtt-client.py

--- mqtt-client.py
+++ mqtt-client.py
@@ -17,7 +17,7 @@ client.on_connect = on_connect
 client.on_message = on_message
 client.enable_logger()
 
-client.connect("3.82.153.249", 1883, 60)
+client.connect("localhost", 1883, 60)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
